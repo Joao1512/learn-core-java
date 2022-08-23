7dfc0842f7797bcf9d93617da3764b3ecfda6479
João Victor Simão <joao.jvsn@pf.gov.br>
2022-08-22 22:56:13 -0300

    breve explicação do objetivo do exercício

--- src/injecao_dependencia_exercicio/Main.java
+++ src/injecao_dependencia_exercicio/Main.java
@@ -7,6 +7,9 @@ public class Main {
     public static void main(String[] args) {
         Contract contract = new Contract(01L, LocalDate.now(), 600L);
         int totalParcelas = 3;
+//        A linha abaixo é uma injeção da dependência TaxasServiceImpl dentro do serviço PaypalService.
+//        A vantagem é diminuir o o acoplamento entre os serviços, caso tenha que mudar TaxasServiceImpl para outra classe, basta alterar este ponto do código
+//        Uma interface foi utilizada, permitindo injetar em  PaypalService qualquer classe que implemente TaxasService
         PaypalService paypalService = new PaypalService(new TaxasServiceImpl());
         paypalService.calcularParcelas(contract, totalParcelas);
         System.out.println(contract.toString());
