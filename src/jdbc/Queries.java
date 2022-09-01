095e053ec0bec545ff3d2f98d9995de3a8d39e7c
João Victor Simão <joao.jvsn@pf.gov.br>
2022-09-01 00:19:15 -0300

    Queries de insert, update e delete.

--- src/jdbc/Queries.java
+++ src/jdbc/Queries.java
@@ -3,4 +3,24 @@ package jdbc;
 public class Queries {
 
     public static final String LIST_DEPARTMENTS = "select * from department";
+    public static final String INSERT_SELLER = "INSERT INTO seller " +
+            "(Name, " +
+            "Email, " +
+            "BirthDate, " +
+            "BaseSalary, " +
+            "DepartmentId)" +
+            "VALUES (" +
+            "?, ?, ?, ?, ?" +
+            ")";
+
+    public static final String UPDATE_SELLER_SALARY = "UPDATE seller" +
+            " SET baseSalary = ?" +
+            " WHERE (" +
+            "Id = ?" +
+            ")";
+
+    public static final String DELETE_SELLER = "DELETE from seller " +
+            "WHERE (" +
+            "Id = ?" +
+            ")";
 }
