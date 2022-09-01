c23852f3ed84abce6924e188b213fe1fb734f3ca
João Victor Simão <joao.jvsn@pf.gov.br>
2022-09-01 00:19:32 -0300

    criando objeto Seller

---
+++ src/jdbc/entities/Seller.java
@@ -0,0 +1,68 @@
+package jdbc.entities;
+
+import java.sql.Date;
+
+public class Seller {
+    private int id;
+    private String name;
+    private String email;
+    private Date birthDate;
+    private Double baseSalary;
+    private int departmentId;
+
+    public Seller(String name, String email, Date birthDate, Double baseSalary, int departmentId) {
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.baseSalary = baseSalary;
+        this.departmentId = departmentId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+}
