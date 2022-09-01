090f6db8ea620dca1bdfd6d169e041ab4f592220
João Victor Simão <joao.jvsn@pf.gov.br>
2022-09-01 00:22:57 -0300

    Simulando chamadas aos DAO`s

--- src/jdbc/main.java
+++ src/jdbc/main.java
@@ -1,24 +1,58 @@
 package jdbc;
 
 import jdbc.DAO.DepartmentDAO;
+import jdbc.DAO.SellerDAO;
+import jdbc.entities.Seller;
 import jdbc.exceptions.DbException;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.LinkedHashSet;
 
 public class main {
-
+    static DepartmentDAO departmentDAO = new DepartmentDAO();
+    static SellerDAO sellerDAO = new SellerDAO();
     public static void main(String[] args) {
-        DepartmentDAO departmentDAO = new DepartmentDAO();
+//        listDepartment();
+//        insertSeller();
+//        updateSalary();
+//        deleteSeller();
+    }
+
+    public static void insertSeller() {
+        Seller seller = mockSeller();
+
+        int rowsAffected = sellerDAO.insertSeller(seller);
+        System.out.println("rows affected: " + rowsAffected);
+    }
+
+    public static void listDepartment() {
+
         LinkedHashSet<String> results = departmentDAO.listDepartments();
         results.forEach(item -> {
             System.out.println(item);
         });
-
     }
 
+    public static Seller mockSeller() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            return new Seller("Joao",
+                    "joao@email.com",
+                    new Date(sdf.parse("15/12/2000").getTime()),
+                    5000.0,
+                    1);
+        } catch (ParseException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
 
+    public static void updateSalary() {
+        sellerDAO.updateSellerSalary(7, 15000.0);
+    }
+
+    public static void deleteSeller() {
+        sellerDAO.deleteSeller(7);
+    }
 }
