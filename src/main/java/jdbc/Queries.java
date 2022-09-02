ee8848f389429adeed8ddafd665071a779c753f8
João Victor Simão <joao.jvsn@pf.gov.br>
2022-09-02 08:41:05 -0300

    Queries findAll e findById

---
+++ src/main/java/jdbc/Queries.java
@@ -0,0 +1,6 @@
+    public static final String FIND_BY_ID = "SELECT * FROM seller " +
+            "WHERE (" +
+            "Id = ?" +
+            ")";
+
+    public static final String FIND_ALL = "SELECT * FROM seller";
